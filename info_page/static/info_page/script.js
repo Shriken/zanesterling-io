e110b9d318b5f82c76d81124c90a31f3929b5ab7
Zane Sterling <sterling.zane@gmail.com>
2017-01-28 11:00:56 -0500

    Add styling for active sections

--- info_page/static/info_page/script.js
+++ info_page/static/info_page/script.js
@@ -1,16 +1,28 @@
+function nav_link_click(event) {
+	document.querySelectorAll('.section')
+		.forEach(function(elem) {
+			elem.style.display = 'none';
+		});
+
+	// Make section visible
+	let id = event.target.href.split('#')[1];
+	let section = document.getElementById(id);
+	section.style.display = 'inline-block';
+
+	// Unstyle old nav-item
+	let old_nav_item = document.querySelector('.nav-link.active');
+	old_nav_item.className = old_nav_item.className.replace(/(?:^|\s)active(?!\S)/g , '');
+
+	// Style nav-item
+	let nav_item = event.target;
+	nav_item.className += ' active';
+}
+
+
 function init() {
 	document.querySelectorAll('.nav-link')
 		.forEach(function(elem) {
-			elem.onclick = function(event) {
-				document.querySelectorAll('.section')
-					.forEach(function(elem) {
-						elem.style.display = "none";
-					});
-
-				let id = event.target.href.split('#')[1];
-				let section = document.getElementById(id);
-				section.style.display = "inline-block";
-			};
+			elem.onclick = nav_link_click;
 		});
 }
 
